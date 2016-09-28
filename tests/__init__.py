822567336d7a957b99e35ac9637d217dd3abf03a
R. David Murray <rdmurray@bitdance.com>
2016-09-28 13:26:26 -0400

    Work around nose's lack of compat with unittest API.
    Nose thinks 'load_tests' is a test and so calls it with no arguments.
    Rather than trying to fix the nose match regex, I'm just detecting
    the call and essentially skipping it.  It adds a bogus passed test
    to the test count, but that hardly matters.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,10 @@
 import os
 
-def load_tests(loader, standard_tests, pattern):
+def load_tests(*args):
+    if not len(args):
+        # Avoid conflict between nose and unittest (nose thinks this is a test).
+        return
+    loader, standard_tests, pattern = args[0], args[1], args[2]
     this_dir = os.path.dirname(__file__)
     if pattern is None:
         pattern = "test*"
